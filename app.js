e1ebb061d547daefeaa02bd4341e068aac2e4e50
john <johngithub33@gmail.com>
2022-01-09 19:53:21 -0500

    Add .

--- app.js
+++ app.js
@@ -3,13 +3,37 @@ var express = require('express')
 var app = express();
 var bodyParser = require('body-parser')
 var mysql = require('mysql')
+var fs = require('fs')
+var ejs = require('ejs')
+var cookieParser = require('cookie-parser'); //don't need cookie-parser for express-session to work
+var session = require('express-session');
+const { connect } = require('http2');
+
+//app.use() - if no route specified first, middleware function called everytime server receives request
 
 // This is a built-in middleware function in Express. 
 // It serves static files and is based on serve-static (a node module)
 //https://expressjs.com/en/resources/middleware/serve-static.html
 //https://www.npmjs.com/package/serve-static
 //the below is the same as app.use(serveStatic(__dirname) if you required ('serve-static))
-app.use(express.static(__dirname)).listen(4000)
+app.use(express.static(__dirname)).listen(4000);
+
+//attaches parsed cookies to req object
+app.use(cookieParser())
+
+//cookie httpOnly: false means that you can then see the cookie with document.cookie with client side JS
+//key: 'connect.sid' can be changed to whatever you want, connect.sid is the default
+app.use(
+    session(
+        {secret:'james secret ssshhhhhhh',
+        cookie: {httpOnly: false},
+        key: 'poopie.sid'}   
+    ));
+
+
+//set the view engine to use the /views folder of html templates
+app.set('view engine', 'ejs');
+
 
 // parse application/x-www-form-urlencoded
 //Returns middleware that only parses urlencoded bodies and only looks at requests 
@@ -23,29 +47,98 @@ app.use(express.static(__dirname)).listen(4000)
 //need to provide extended option due to deprecation
 app.use(bodyParser.urlencoded({ extended: false }))
 
-app.post('/formsubmission', (req,res) => {
-    console.log(
-        req.url,
-        req.method,
-        req.body,
-        req.body['fname'],
-        req.body['email']
-    )
 
-    let con = mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        port: '3306',  /* port on which phpmyadmin run */
-        password: 'password'
-        // socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock', //for mac and linux
-        //database: "jamestrial" 
-    });
+app.get('/cookietest', (req,res) => {
+    
+    // res.cookie('name', 'express').send('cookie set'); //Sets name = express
+    // res.cookie('name', 'jamescookie').send('cookie set');
+    // res.cookie('othername', 'ricky123').send('cookie set')
+    // res.cookie('thirdnamecookie', 'thirdvalue123123123').send('cookie set')
+    // res.cookie('timedcookie', 'timernvalue', {maxAge: 6000}).send('cookie set')
+    
+    if(req.session.pagevws){
+        req.session.pagevws++;
+        res.send('multiple times here')
+        console.log(session.MemoryStore)
+        console.log(req.session, req.cookies['connect.sid'])
+        console.log('req.session.id:', req.session.id)
+        console.log('req.sessionID:', req.sessionID)
+        console.log('req.session.cookie:', req.session.cookie)
+    }
+    else{
+        req.session.pagevws = 1;
+        res.send('first time here')
+        // console.log(req.session);
+        // console.log(req.cookies['connect.sid'])
+    }
 
-    con.query('USE jamestrial');
-    con.query("SELECT * FROM sessions;", function (err,result){
-        console.log("THIS IS RESULT: ", result);
-    });    
 
-    // res.end('you submitted!!!')
-    res.sendFile('formsubmission.html', {root:__dirname});
+    // console.log(req.cookies)
 })
+
+
+app.post('/formsubmission', (req,res) => {
+
+    let con = mysql.createConnection({host: 'localhost', user: 'root', port: '3306', password: 'password'});
+    
+    con.query('USE jamestrial');
+    con.query("SELECT * FROM sessions;", function(err,result) {
+        
+        printToPage(res, req, JSON.stringify(result[0]))
+
+    });    
+    
+})
+
+function printToPage(res, req, myResult){
+
+    
+    let localReq = req.body['fname'];
+    
+    let choice = '5';
+    
+    switch(choice){
+        case '1':
+            res.write('<html>');
+            //res.write('<head> <title> Hello TutorialsPoint </title> </head>');
+            res.write('<body> Hello Tutorials Point');
+            res.write(`<p>${myResult}</p>`);
+            res.write(`<p>${req.method}</p>`);
+            res.write(`<p>${req.url}</p>`);
+            res.write(`<p>${localReq}</p>`); 
+            res.write('</body>')
+            res.write('</html>');
+            //write end to mark it as stop for node js response.
+            res.end();
+
+        case '2':
+            if(!res.headerSent) {
+                res.send('choice 2');
+            }
+
+        case '3':
+            if(!res.headerSent){
+                res.sendFile('formsubmission.html', {root:__dirname});
+            }
+
+        case '4':
+            if(!res.headerSent){
+                fs.appendFile('formsubmission.html',"hello again!", (err) => {
+                    if(err) throw err;
+                    // else fs.readFileSync('formsubmission.html', 'utf8')
+                    else res.sendFile('index.html', {root:__dirname})
+                });
+            }
+
+        //ejs views
+        case '5':
+            if(!res.headerSent){
+                res.render('home', {ejsVariable: localReq})
+                
+            }
+
+            
+    }
+
+}
+
--- public/script.js
+++
@@ -1,5 +0,0 @@
-
-
-var pr = (x) => console.log(x);
-var qs = (x) => document.querySelector(x);
-var emailRgxStr = /\S+@\S+[.]\S/;
